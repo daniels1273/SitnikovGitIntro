afd4d4a3d161b68fbd18deb5f1903de59a2baa16
BT_1N3_27 <sample@bths.edu>
2025-11-20 10:26:17 -0500

    added scanner stuff

--- src/RunnerHahaha.java
+++ src/RunnerHahaha.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class RunnerHahaha {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
         System.out.println("Hahaha!!");
         System.out.println("Man this is too easy");
         boolean isEasy = true;
         int xAmount = 0;
         int yAmount = 0;
-        if (isEasy){
+
+        System.out.print("Is this actually easy tho? ");
+        boolean isEasyReal = scan.hasNextBoolean();
+
+        if (isEasyReal){
             xAmount = 12;
         } else {
-            yAmount = 789356785;
+            yAmount = 7896785;
         }
-        System.out.println(xAmount>yAmount);
+
+        System.out.println("a" + isEasyReal + xAmount + yAmount);
     }
 }
