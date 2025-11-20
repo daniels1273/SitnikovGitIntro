a462e7c22f86d8c52e7bbc51c0b876d6da4e132a
BT_1N3_27 <sample@bths.edu>
2025-11-20 10:21:30 -0500

    added logic and fun stuff

--- src/RunnerHahaha.java
+++ src/RunnerHahaha.java
@@ -1,5 +1,15 @@
 public class RunnerHahaha {
     public static void main(String[] args) {
-        System.out.println("Hahaha!");
+        System.out.println("Hahaha!!");
+        System.out.println("Man this is too easy");
+        boolean isEasy = true;
+        int xAmount = 0;
+        int yAmount = 0;
+        if (isEasy){
+            xAmount = 12;
+        } else {
+            yAmount = 789356785;
+        }
+        System.out.println(xAmount>yAmount);
     }
 }
