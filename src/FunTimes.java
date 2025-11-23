e3a855c73dc51b46e705c8b14cd1c40d6de2aad8
Daniel Sitnikov <dannycoolboy1324@gmail.com>
2025-11-23 15:13:18 -0500

    added vars and print statements to FunTimes at home

--- src/FunTimes.java
+++ src/FunTimes.java
@@ -1,5 +1,13 @@
 public class FunTimes {
     public static void main(String[] args) {
         System.out.println("Who's ready for some... FUN TIMES??? JAHAHAHAAUIOSDISDG");
+        System.out.println("Look! I'm doing this at home! I'm having a... FUN TIME SDFIOSDIPOFJPOSDOJIPFOOPI");
+        System.out.println("I should make more changes right?");
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        System.out.println(a);
+        System.out.println(b+a);
+        System.out.println(c+c+c+c+c+c+c+c+c+c);
     }
 }
