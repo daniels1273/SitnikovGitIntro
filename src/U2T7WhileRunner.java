26e4b02870e966218a05c11cd201e28ff2921a0a
BT_1N3_27 <sample@bths.edu>
2025-11-24 11:03:56 -0500

    Did most stuff for lab, finish it you bum

---
+++ src/U2T7WhileRunner.java
@@ -0,0 +1,7 @@
+public class U2T7WhileRunner {
+    public static void main(String[] args) {
+        WhileLoopFun test = new WhileLoopFun();
+        test.printDigits(150856);
+        System.out.println(test.countLetter("apple","p"));
+    }
+}
