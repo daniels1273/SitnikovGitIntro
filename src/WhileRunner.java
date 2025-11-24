b064e3206a193f1a01013a3840ac18e744106d83
BT_1N3_27 <sample@bths.edu>
2025-11-24 10:07:01 -0500

    changes...

---
+++ src/WhileRunner.java
@@ -0,0 +1,9 @@
+public class WhileRunner {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}
